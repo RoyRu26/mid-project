7f40581c9b691d9de4b20c5c22ef64976d667919
RoyRu26 <roy.rutzky@gmail.com>
2023-01-11 16:33:30 +0200

    endOfWednesday

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,7 +1,9 @@
 import './getstarted.css'
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Box, styled } from '@mui/system';
-import { Input, InputBase, TextField, Typography } from '@mui/material';
+import { Autocomplete, Card, CardContent, CardMedia, Input, InputBase, TextField, Typography } from '@mui/material';
+import { actList } from '../App';
+import { NavLink } from 'react-router-dom';
 
 const Search = styled('div')({
     backgroundColor: 'white',
@@ -11,14 +13,33 @@ const Search = styled('div')({
     height: '10%',
 })
 function GetStarted(props) {
-    const [country, setCountry] = useState('')
+    // const [country, setCountry] = useState('')
+    const { actArr } = useContext(actList)
+    // const countries = ['South Africa', 'Costa Rica', 'Brazil', 'Peru', 'Tanzania', 'Ecuador', 'Indonesia', 'Thailand', 'Nepal']
     return (
         <div className='container'>
-            <Box height='80%' width='40%' bgcolor='white' borderRadius='4%' marginTop='5%' display='flex' flexDirection='column' justifyContent='space-evenly'>
-                <Typography color='secondary'>Choose a country</Typography>
-                <Input type='text' placeholder='counry' sx={{border:'3% solid black'}} onChange={(e) => setCountry(e.target.value)}/>
+            <Box height='80%' width='40%' bgcolor='white' borderRadius='4%' marginTop='5%' display='flex' flexDirection='column' justifyContent='space-evenly' alignItems='center'>
+                <Typography fontSize='large' marginBottom='2%' textAlign='center' fontWeight='medium' color='secondary'>Choose a Country</Typography>
+                {/* <Autocomplete
+                        disablePortal
+                        options={countries}
+                        sx={{ width: '30vw' }}
+                        onChange = {(event,value) => setCountry(value)}
+                        renderInput={(params) => <TextField {...params} la="country" />}
+                    /> */}
+                <Box width='100%' height='80%' display='flex' justifyContent='center' flexDirection='row' flexWrap='wrap'  >
+                    <NavLink to=''><div id='flagDiv' className='SA'><p className='flagText'>South Africa</p></div></NavLink>
+                    <NavLink to=''><div id='flagDiv' className='Thailand'><Typography className='flagText'>Thailand</Typography></div></NavLink>
+                    <NavLink to=''><div id='flagDiv' className='Costa'><Typography className='flagText'>Costa Rica</Typography></div></NavLink>
+                    <NavLink to=''><div id='flagDiv' className='Nepal'><Typography className='flagText'>Nepal</Typography></div></NavLink>
+                    <NavLink to=''><div id='flagDiv' className='Peru'><Typography className='flagText'>Peru</Typography></div></NavLink>
+                    <NavLink to=''><div id='flagDiv' className='Tansania'><Typography className='flagText'>Tansania</Typography></div></NavLink>
+                    <NavLink to=''><div id='flagDiv' className='Ecuador'><Typography className='flagText'>Ecuador</Typography></div></NavLink>
+                    <NavLink to=''><div id='flagDiv' className='Indonesia'><Typography className='flagText'>Indonesia</Typography></div></NavLink>
+                    <NavLink to=''><div id='flagDiv' className='Brazil'><Typography className='flagText'>Brazil</Typography></div></NavLink>
+                </Box>
             </Box>
-        </div>
+        </div >
     );
 }
 
